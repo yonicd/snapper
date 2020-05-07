e47b3cc00ccb925a6cc010b94cdd758548163b1f
Jonathan Sidi <yonicd@gmail.com>
2020-05-07 06:55:23 -0400

    refactor load_snapper to let user load html2canvas and jQuery

--- R/load.R
+++ R/load.R
@@ -1,8 +1,15 @@
-#' @title Load snapper JS libraries in ShinyApp
-#' @description Load html2canvas jquery library and download script
-#'  in Shiny App environment.
+#' @title Attach snapper dependencies to the Shiny App
+#' @description Attach html2canvas and jQuery javascript libraries and
+#' download script to the Shiny App environment.
 #' @param html2canvas character, url path to html2canvas library.
 #' Default: 'https://html2canvas.hertzen.com/dist/html2canvas.js'
+#' @param jquery character, url path to jQuery library.
+#' Default: 'https://code.jquery.com/jquery-3.5.0.min.js'
+#' @details
+#' If your app needs to work with clients that won't be able to connect to the
+#' wider internet, you'll need to download the javascript files,
+#' put it in an app subdirectory (say, html2canvas),
+#' and point to the arguments to the their respective paths.
 #' @return shiny.tag.list
 #' @inherit preview_button examples
 #' @seealso
@@ -11,9 +18,18 @@
 #' @family load
 #' @export
 #' @importFrom shiny tagList tags
-load_snapper <- function(html2canvas = 'https://html2canvas.hertzen.com/dist/html2canvas.js'){
+#' @importFrom  htmltools htmlDependency
+load_snapper <- function(
+  html2canvas = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
+  jquery = 'https://code.jquery.com/jquery-3.5.0.min.js'){
+
   shiny::tagList(
-    shiny::tags$script(src = html2canvas),
+    htmltools::htmlDependency("jquery", "3.5.0",
+                              src = c(href = dirname(jquery)),
+                              script = basename(jquery)),
+    htmltools::htmlDependency("html2canvas", "1.0.0",
+                              src = c(href = dirname(html2canvas)),
+                              script = basename(html2canvas)),
     shiny::tags$script('
       var saveAs = function(uri, filename) {
             var link = document.createElement("a");
